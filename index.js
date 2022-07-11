bced12f65c107321d0fd7dd5404ab695c087a02a
Sszyh <zhuyer@outlook.com>
2022-07-10 21:23:44 -0700

    code without transaction history

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
+  constructor(username) {
+    this.username = username;
 
-  constructor(amount) {
+    this.balance = 0;
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
+
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value
   }
-
 }
 
+class Withdrawal extends Transaction{
+  get value() {
+    return -this.amount;
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+}
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("Hi newbee");
+myAccount.balance = 400;
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log(myAccount.balance);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
